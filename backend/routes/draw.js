3b68a2951257d5c914e2c1fb6d45eef5eae59616
AfrinDange <dangeafrin@gmail.com>
2020-07-25 19:31:56 +0530

    Adding Final Files

--- backend/routes/draw.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-router.route('/').get((req, res) => {
-    res.send('DRAW PAGE!');
-});
-
-module.exports = router;
--- backend/routes/game.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-router.route('/').get((req,res) => {
-    res.send('GAME PAGE!')
-});
-
-module.exports = router;
