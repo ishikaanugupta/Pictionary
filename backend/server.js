42ada8e40adfe02c8c7a9bb98b2d54ef1cd0c1d4
Afrin Dange <44875588+AfrinDange@users.noreply.github.com>
2020-07-25 19:23:02 +0530

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -2,38 +2,32 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-require('dotenv').config();
-
 const app = express();
 const port = process.env.PORT || 5000;
-let doodleCategories = []
 
-const url = 'mongodb://127.0.0.1:27017/Doodles'
-mongoose.connect(url, { useNewUrlParser: true })
-mongoose.connection.once('open', _ => {
-    console.log('Database connected: ', url)
-    mongoose.connection.db.listCollections().toArray(function (err, names) {
-        names.forEach(element => {
-            doodleCategories.push(element.name)
-        });
-        console.log(doodleCategories);
-        console.log("DEAR LORD!")
-        exports.doodleCategories = doodleCategories; 
-    });
-})
+//const url = 'mongodb://127.0.0.1:27017/Doodles'
+//mongoose.connect(url, { useNewUrlParser: true })
+//mongoose.connection.once('open', _ => {
+//    console.log('Database connected: ', url)
+//})
+//mongoose.connection.on('error', err => {
+//    console.error('connection error: ', err)
+//})
 
-mongoose.connection.on('error', err => {
-    console.error('connection error: ', err)
-})
+const uri = "mongodb+srv://afrin:AccessGranted@cluster0.owtuu.mongodb.net/Doodles?retryWrites=true&w=majority";
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then( () => {
+        console.log('Database Connected!')
+    })
+    .catch( err => console.log('Database Not Connected: ', err))
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const gameRouter = require('./routes/game');
 const drawRouter = require('./routes/draw');
 const guessRouter = require('./routes/guess');
 
-app.use('/', gameRouter);
 app.use('/draw', drawRouter);
 app.use('/guess', guessRouter);
 
