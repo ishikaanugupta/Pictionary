af920d9f1250421d9a36882bb14952560149db7a
Afrin Dange <44875588+AfrinDange@users.noreply.github.com>
2020-07-28 00:01:06 +0530

    Fixed

--- src/components/game-option.component.js
+++ src/components/game-option.component.js
@@ -58,7 +58,7 @@ export default class GameOption extends Component {
                             </span> 
                         </p>
                 </footer>
-    Name         </div>
+            </div>
         );
     }
 }
